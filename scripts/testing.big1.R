094115a52eff694620e0e2920cc91720b9447476
Samuel Jenness <sjenness@uw.edu>
2016-07-05 12:36:43 -0700

    Add follow-up example to testbig script

--- scripts/testing.big1.R
+++ scripts/testing.big1.R
@@ -5,20 +5,37 @@ library("EpiModelHPC")
 
 load("est/nwstats.10k.rda")
 
-param <- param_msm(nwstats = st, 
+param <- param_msm(nwstats = st,
                    ai.scale = 1.323,
                    prep.coverage = 0)
-init <- init_msm(nwstats = st, 
-                 prev.B = 0.253, 
+init <- init_msm(nwstats = st,
+                 prev.B = 0.253,
                  prev.W = 0.253)
-control <- control_msm(simno = 0.253, 
-                       nsteps = 52 * 10,
-                       nsims = 16, 
-                       ncores = 16, 
+control <- control_msm(simno = 1,
+                       nsteps = 200,
+                       nsims = 16,
+                       ncores = 16,
                        verbose.int = 1)
 
 ## Simulation
-load("est/fit.10k.rda")
-sim <- netsim(est, param, init, control)
+# load("est/fit.10k.rda")
+# sim <- netsim(est, param, init, control)
 
-netsim_hpc("est/fit.10k.rda", param, init, control)
+netsim_hpc("est/fit.10k.rda", param, init, control, cp.save.int = 100,
+            save.min = FALSE, save.max = TRUE)
+
+
+# follow up
+param <- param_msm(nwstats = st, ai.scale = 1.323, prep.coverage = 0)
+init <- init_msm(nwstats = st)
+control <- control_msm(simno = 1,
+                       start = 201,
+                       nsteps = 400,
+                       nsims = 16,
+                       ncores = 16,
+                       save.other = NULL,
+                       initialize.FUN = reinit_msm)
+
+## Simulation
+netsim_hpc("data/sim.n1.20160705.1230.rda", param, init, control,
+           save.min = TRUE, save.max = FALSE)
